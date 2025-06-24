d711f22f2f19973be2bd3f42cf0ca5cf0c9274f4
YUN-SEOK LEE <115852659+lyseok@users.noreply.github.com>
2025-06-24 16:51:45 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 256 ms, Memory: 25852 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int T;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true) {
+			String end = br.readLine();
+			if(end.equals("."))
+				break;
+			StringTokenizer st = new StringTokenizer(end,"[]()",true);
+			Stack<String> stk = new Stack<>();
+			boolean flag = true;
+			while(st.hasMoreTokens()) {
+				String temp = st.nextToken();
+				if(temp.equals("(") || temp.equals("[")) {
+					stk.push(temp);
+				}else if(temp.equals(")")) {
+					if(stk.isEmpty()) {
+						flag = false;
+						break;
+					}
+					if(stk.peek().equals("("))
+						stk.pop();
+					else {
+						flag = false;
+						break;
+					}
+				}else if(temp.equals("]")) {
+					if(stk.isEmpty()) {
+						flag = false;
+						break;
+					}
+					if(stk.peek().equals("["))
+						stk.pop();
+					else {
+						flag = false;
+						break;
+					}
+				}
+			}
+			if(flag && stk.isEmpty())
+				sb.append("yes").append("\n");
+			else
+				sb.append("no").append("\n");
+		}
+		System.out.println(sb);
+		}
+}
